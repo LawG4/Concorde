ebdc49141a04f7825b50852f6b95387a9c2a22ec
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-06 20:42:25 +0100

    Build: Let user specify their build machine arch

--- Config.cmake
+++ Config.cmake
@@ -1,6 +1,8 @@
 # Variables needed for Desktop Development
+# Host system's arch to pull the right lib: x86, x64
+set(CONC_HOST_ARCH "x64")
 # Needed for cross platform window system
-set(SDL_PATH )
+set(SDL_PATH "")
 
 # Variables needed for Wii Development
 set(CONC_DOLPHIN_CMD dolphin_emu)
