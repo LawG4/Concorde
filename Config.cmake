8f798ea498e3a8bdec80c8740d2fecde4d007778
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-06 20:16:51 +0100

    Build: Add Config file for user to customise their builds

---
+++ Config.cmake
@@ -0,0 +1,9 @@
+# Variables needed for Desktop Development
+# Needed for cross platform window system
+set(SDL_PATH )
+
+# Variables needed for Wii Development
+set(CONC_DOLPHIN_CMD dolphin_emu)
+set(DEVKITPRO /opt/devkitpro)
+set(DEVKITARM /opt/devkitpro/devkitARM)
+set(DEVKITPPC /opt/devkitpro/devkitPPC)
