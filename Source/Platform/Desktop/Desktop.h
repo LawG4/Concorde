0f2e6c6f9d907e9f8d8c9c2d121caea6121f6ce9
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-07 22:31:42 +0100

    Source: Add desktop header
    Allows forward declaring of useful variables like the SDL window handle

---
+++ Source/Platform/Desktop/Desktop.h
@@ -0,0 +1,14 @@
+/**
+ * All of the variables that we need to pass amungst the desktop platforms
+ */
+#ifndef CONCORDE_DESKTOP_HEADER
+#define CONCORDE_DESKTOP_HEADER
+
+#include <SDL.h>
+
+/**
+ * An opaque handle for the SDL window
+ */
+extern SDL_Window *window;
+
+#endif // !CONCORDE_DESKTOP_HEADER
