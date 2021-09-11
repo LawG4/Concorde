95c29d2ded4d9621544ddf94133ebe4508aad849
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-11 17:01:54 +0100

    Build: Move language enabling to after toolchain selection
    When CMake enables a language, it does a compiler test to ensure the selected toolchain works. by defualt project enables languages C and CXX, so we need to ensure the user passes none into the languages, and then the compiler tests are done after we have loaded the toolchain

--- CMake/Toolchains/Platforms.cmake
+++ CMake/Toolchains/Platforms.cmake
@@ -20,7 +20,8 @@ if(NOT ${PLATFORM_WII_SUBSTRING} EQUAL -1)
 	message("Configuring build for Wii")
 	set(PLATFORM "PLATFORM_WII")
 
-	# Include the wii toolchain
+	# Set the toolchain file to wii
+	set(CMAKE_TOOLCHAIN_FILE "${PROJECT_SOURCE_DIR}/CMake/Toolchains/Wii.cmake")
 else()
 	# Not detected wii, assume desktop
 	message("Configuring build for Desktop")
@@ -31,3 +32,7 @@ endif()
 
 # Cache the platform variable
 set(CACHE_PLATFORM "${PLATFORM}" CACHE STRING "Concorde declaration storing the chosen target platform from command line options" FORCE)
+
+# Set the languages to C and Cxx
+enable_language(CXX)
+enable_language(C)
