d5df2f9e37f5c35434e55428cf0a62b6a159d745
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-22 23:13:24 +0100

    Inputs: Return key bitmasks to external users of concorde

--- Source/Input/Concorde_Internal_Inputs.h
+++ Source/Input/Concorde_Internal_Inputs.h
@@ -1,15 +1,18 @@
 /**
-* Rather than wasting space on the call stack space to set buttons
-* Just expose the variables directly to source files inside concorde
-*/
+ * Rather than wasting space on the call stack space to set buttons
+ * Just expose the variables directly to source files inside concorde
+ */
 #ifndef __CONCORDE_INT_INPUTS_H__
 #define __CONCORDE_INT_INPUTS_H__
 #include "Concorde.h"
 
+/*Buttons that have been pressed in the most recent input scan*/
 extern uint32_t buttons_downed_bit_mask;
 
+/*Buttons currently held down*/
 extern uint32_t buttons_down_bit_mask;
 
-extern uint32_t buttons_up_bit_mask;
+/*Buttons that have been released in the most recent input scan*/
+extern uint32_t buttons_upped_bit_mask;
 
 #endif // !__CONCORDE_INT_INPUTS_H__
--- Source/Input/Inputs.c
+++ Source/Input/Inputs.c
@@ -5,23 +5,23 @@
 #include "Concorde_Internal_Inputs.h"
 /*Internal variables that are used for keeping track*/
 
-uint32_t buttons_downed_bit_mask = 0;
-uint32_t buttons_down_bit_mask = 0;
-uint32_t buttons_up_bit_mask = 0;
+uint32_t buttons_downed_bit_mask;
+uint32_t buttons_down_bit_mask;
+uint32_t buttons_upped_bit_mask;
 
 uint32_t concorde_buttons_downed()
 {
-    return CONCORDE_SUCCESS;
+    return buttons_downed_bit_mask;
 }
 
 uint32_t concorde_buttons_currently_down()
 {
-    return CONCORDE_SUCCESS;
+    return buttons_down_bit_mask;
 }
 
 uint32_t concorde_buttons_up()
 {
-    return CONCORDE_SUCCESS;
+    return buttons_upped_bit_mask;
 }
 
 uint8_t concorde_joystick_state(uint8_t playerID, uint8_t stickID, float *x, float *y)
