e8a734694901f34f9216aaec2d1348284bfa0336
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-26 22:23:46 +0100

    Render: Add common files for rendering

---
+++ Source/Render/Concorde_Internal_Render.h
@@ -0,0 +1,11 @@
+/**
+ * Rather than wasting space on the call stack space to set buttons
+ * Just expose the variables directly to source files inside concorde
+ */
+#ifndef __CONCORDE_INT_RENDER_H__
+#define __CONCORDE_INT_RENDER_H__
+
+#include "Concorde.h"
+#include "Concorde_Render.h"
+
+#endif  // !__CONCORDE_INT_RENDER_H__
---
+++ Source/Render/Render.c
@@ -0,0 +1,6 @@
+/**
+ * Writes the common code for the rendering systems
+ */
+#include "Concorde.h"
+#include "Concorde_Internal_Render.h"
+#include "Concorde_Render.h"
