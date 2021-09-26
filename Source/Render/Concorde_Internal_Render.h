6f45bb0902849d9b80613a0d9431cfe10d27d769
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-26 22:53:28 +0100

    Render: Start writing the internal wrappers for render

--- Source/Render/Concorde_Internal_Render.h
+++ Source/Render/Concorde_Internal_Render.h
@@ -1,11 +1,19 @@
 /**
- * Rather than wasting space on the call stack space to set buttons
+ * Rather than wasting space on the call stack space to set render state
  * Just expose the variables directly to source files inside concorde
  */
 #ifndef __CONCORDE_INT_RENDER_H__
 #define __CONCORDE_INT_RENDER_H__
 
+#include <stdbool.h>
+
 #include "Concorde.h"
 #include "Concorde_Render.h"
+
+/*Is concorde currently trying to render?*/
+extern bool Concorde_Rendering;
+
+/*How many vertices remain to be drawn before concorde can end*/
+extern uint32_t Concorde_Vert_Remaining;
 
 #endif  // !__CONCORDE_INT_RENDER_H__
--- Source/Render/Render.c
+++ Source/Render/Render.c
@@ -4,3 +4,44 @@
 #include "Concorde.h"
 #include "Concorde_Internal_Render.h"
 #include "Concorde_Render.h"
+
+/*Define the forward declared render state tracking variables*/
+bool Concorde_Rendering = false;
+uint32_t Concorde_Vert_Remaining = 0;
+
+/*Provide a wrapper for the external rendering functions*/
+
+concorde_render_error_codes concorde_render_begin(
+    concorde_rendering_mode rendering_mode, concorde_primative primative,
+    concorde_vertex_mask vertex_mask, uint32_t vertex_count) {
+  /*Check concorde isn't already rendering something else*/
+  if (Concorde_Rendering) {
+    return Already_Rendering;
+  }
+  Concorde_Rendering = true;
+  Concorde_Vert_Remaining = vertex_count;
+
+  /*Call into the platform specific rendering function*/
+  /*Todo implement this bad boi*/
+
+  return Success;
+}
+
+concorde_render_error_codes concorde_render_end(void) {
+  /*Is concorde even rendering?*/
+  if (!Concorde_Rendering) {
+    return Success;
+  }
+
+  /*Has the user submitted enough vertices?*/
+  if (Concorde_Vert_Remaining != 0) {
+    /*Somehow recover from this?*/
+  }
+
+  /*Call the platform specific end rendering function*/
+
+  /*Exit out*/
+  Concorde_Rendering = false;
+  Concorde_Vert_Remaining = 0;
+  return Success;
+}
