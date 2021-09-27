5cdead3545f85aaf161b9333f236de017af88cbd
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-27 23:10:13 +0100

    Example: Add in immediate rendering of 3 vertices to base example

--- Examples/02_HelloTriangle/Main.c
+++ Examples/02_HelloTriangle/Main.c
@@ -1,25 +1,52 @@
+#include <stdio.h>
+
 #include "Concorde.h"
 
-int main()
-{
-    /*Initialise concorde with default values*/
-    concorde_init_info init = default_init_info;
-    concorde_init(&init);
+int main() {
+  /*Initialise concorde with default values*/
+  concorde_init_info init = default_init_info;
+  concorde_init(&init);
 
-    /*Enter into the windowing loop*/
-    while (!concorde_should_close())
-    {
-        /*Process inputs, including should the window close*/
-        concorde_scan_inputs();
+  /*Create one list for each vertex component we'd like to use*/
+  float triangle_vert_pos[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+  float triangle_vert_col[3][3] = {{0, 0, 1}, {0, 1, 0}, {1, 0, 0}};
 
-        /*Detect if the user has pressed the home button and break*/
-        if (concorde_buttons_currently_down() & CONCORDE_KEY_HOME)
-            break;
+  /*Create a mask we will use to tell concorde that each vertex is contains only
+   * position and colour data*/
+  concorde_vertex_mask vertex_mask = cvm_position | cvm_color;
 
-        /*Swap the framebuffers*/
-        concorde_swap_buffers();
+  /*Enter into the windowing loop*/
+  while (!concorde_should_close()) {
+    /*Process inputs, including should the window close*/
+    concorde_scan_inputs();
+
+    /*Detect if the user has pressed the home button and break*/
+    if (concorde_buttons_currently_down() & CONCORDE_KEY_HOME) break;
+
+    /*This demo will use immediate rendering, submit one vertex at a time*/
+    concorde_render_begin(crm_immediate, 3, vertex_mask, cp_triangle);
+
+    /*Submit the verices one at a time*/
+    for (uint8_t i = 0; i < 3; i++) {
+      concorde_immediate_vertex(cvm_position, triangle_vert_pos[i][0],
+                                triangle_vert_pos[i][1],
+                                triangle_vert_pos[i][2]);
+      concorde_immediate_vertex(cvm_color, triangle_vert_col[i][0],
+                                triangle_vert_col[i][1],
+                                triangle_vert_col[i][2]);
     }
 
-    /*Clean up concorde*/
-    concorde_deint();
+    /*End the rendering call*/
+    concorde_render_error_codes err;
+    if (err = concorde_render_end() != crec_success) {
+      printf("Something went wrong rendering your triangle :%i\n", err);
+      break;
+    }
+
+    /*Swap the framebuffers*/
+    concorde_swap_buffers();
+  }
+
+  /*Clean up concorde*/
+  concorde_deint();
 }
