b8645b2faee78d2d78713dfea604d05d081de98e
Lawrence <Lawrence.martin.green@gmail.com>
2021-09-27 22:33:20 +0100

    Examples: Template example 2 for drawing triangles

---
+++ Examples/02_HelloTriangle/Main.c
@@ -0,0 +1,25 @@
+#include "Concorde.h"
+
+int main()
+{
+    /*Initialise concorde with default values*/
+    concorde_init_info init = default_init_info;
+    concorde_init(&init);
+
+    /*Enter into the windowing loop*/
+    while (!concorde_should_close())
+    {
+        /*Process inputs, including should the window close*/
+        concorde_scan_inputs();
+
+        /*Detect if the user has pressed the home button and break*/
+        if (concorde_buttons_currently_down() & CONCORDE_KEY_HOME)
+            break;
+
+        /*Swap the framebuffers*/
+        concorde_swap_buffers();
+    }
+
+    /*Clean up concorde*/
+    concorde_deint();
+}
