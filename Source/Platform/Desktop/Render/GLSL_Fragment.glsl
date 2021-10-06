43b2970336f049da79ad59742639dfb93d379e88
Lawrence <Lawrence.martin.green@gmail.com>
2021-10-07 00:13:43 +0100

    Render: GL Shaders Add in a color component

--- Source/Platform/Desktop/Render/GLSL_Fragment.glsl
+++ Source/Platform/Desktop/Render/GLSL_Fragment.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
+// Recieve the interpolated vertex attributes from the vertex shader
+in vec3 vCol;
+
+// Write out the fragment color to the framebuffer
 out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	fragColor = vec4(vCol.x, vCol.y, vCol.z, 1.0);
 }
--- Source/Platform/Desktop/Render/GLSL_Vertex.glsl
+++ Source/Platform/Desktop/Render/GLSL_Vertex.glsl
@@ -1,8 +1,17 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos;
+// Recieve vertex attributes from the CPU
+in vec3 aPos;
+in vec3 aCol;
 
+// Pass the required attributes onto fragment shader
+out vec3 vCol;
+
+//Enter into the main function 
 void main()
 {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+
+	// Pass on the variables
+	vCol = aCol;
 }
