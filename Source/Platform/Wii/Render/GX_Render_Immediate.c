d47832c11bbf87e7ef7ac9671080c86f3a8170d9
Lawrence <Lawrence.martin.green@gmail.com>
2021-10-10 22:59:38 +0100

    Render: Pass on immediate coordinates to gx

--- Source/Platform/Wii/Render/GX_Render_Immediate.c
+++ Source/Platform/Wii/Render/GX_Render_Immediate.c
@@ -5,16 +5,20 @@
  */
 #include "Concorde_Internal_Render.h"
 
+#include <gccore.h>
 
+#include <stdio.h>
 
 concorde_render_error_codes platform_immediate_render_pos(float x, float y,
                                                           float z)
 {
+    GX_Position3f32(x, y, z);    
     return crec_success;
 }
 
 concorde_render_error_codes platform_immediate_render_col(float R, float G,
                                                           float B, float A)
 {
+    GX_Color3f32(R, G, B);
     return crec_success;
 }
