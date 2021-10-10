c43c9f4323c61529b109d69ba5360e5a4608b0a7
Lawrence <Lawrence.martin.green@gmail.com>
2021-10-10 23:31:08 +0100

    Render: Renderer init sets some very basic assumed vertex formats and matrix data

--- Source/Platform/Wii/Render/GX_Render.c
+++ Source/Platform/Wii/Render/GX_Render.c
@@ -10,6 +10,18 @@
 
 concorde_render_error_codes init_gx_renderer(void)
 {
+    /*When we start the Concorde renderer, we're assuming that the user will be using direct
+    rendering with only colour and position data for each vertex. If they want to change this
+    then they will be able to do this, with commands which haven't been defined yet.
+    *    We're starting the renderer with the assumption of a simple vertex format so that 
+    users just trying out concorde can get up and running with less set up.*/
+    GX_ClearVtxDesc();
+    GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+    GX_SetVtxDesc(GX_VA_CLR0, GX_DIRECT);
+
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGB8, 0);
+
     /*When rendering, the hardware can rasterise up to 2 different render targets 
     That is then fed into the TEV stage, which mixes those colours in some way.
     We initialise the renderer with 1 output target 
@@ -29,6 +41,24 @@ concorde_render_error_codes init_gx_renderer(void)
     to just pass the color pixel onto the framebuffer*/
     GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORDNULL, GX_TEXMAP_NULL, GX_COLOR0A0);
     GX_SetTevOp(GX_TEVSTAGE0, GX_PASSCLR);
+
+    /*Once again the renderer init has some assumptions about the users input 
+    style for their vertices, we assume that they're just trying to submit
+    screenspace coordinates, so we load dummy position and projection mats*/
+    Mtx44 projec;
+    float aspectRatio = (float)videoMode->viWidth / (float)videoMode->viHeight;
+    guPerspective(projec, 45, aspectRatio, 0.1f, 300.0f);
+    GX_LoadProjectionMtx(projec, GX_PERSPECTIVE);
+
+    guVector cam = {0.0F, 0.0F, 0.0F},
+			up = {0.0F, 1.0F, 0.0F},
+		  look = {0.0F, 0.0F, -1.0F};
+    Mtx viewMat;
+    guLookAt(viewMat, &cam, &up, &look);
+    guMtxTransApply(viewMat, viewMat, 0.0f, 0.0f, -2.5f);
+
+    GX_LoadPosMtxImm(viewMat, GX_PNMTX0);
+
     return crec_success;
 }
 
@@ -36,13 +66,21 @@ concorde_render_error_codes platform_render_begin(
     concorde_rendering_mode rendering_mode, concorde_primative primative,
     concorde_vertex_mask vertex_mask, uint32_t vertex_count)
 {
+    /*TODO: */
+    /*We need to implement a system that will switch up the vertex formats depending
+    on what information the user is submitting here, right now this only works under
+    the assumption that the user is submitting vertices in the default manner*/
+
     /*Start the gx render, this promises to gx the number of vertices we'll be submitting
     If we don't submit enough then GX will hang*/
+    GX_Begin(GX_TRIANGLES, GX_VTXFMT0, vertex_count);
 
     return crec_success;
 }
 
 concorde_render_error_codes platform_render_end()
 {
+    /*End the GX render cycle, remember if we haven't submitted enough vertices GX will hang*/
+    GX_End();
     return crec_success;
 }
